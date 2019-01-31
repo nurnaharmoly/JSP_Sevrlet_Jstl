f2eb6011bebf63ef0634174cc8a7762bf345b292
nurhaharmoly <nurnaharmoly@gmail.com>
2019-01-31 12:59:10 +0600

    evi

---
+++ All.JSP.File/jsf/jsfHibernate37roundCrud/src/java/com/service/StudentService.java
@@ -0,0 +1,58 @@
+
+package com.service;
+
+import com.entity.Student;
+import com.util.HibernateUtil;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class StudentService {
+    
+    public static void saveOrUpdate(Student student){
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        session.saveOrUpdate(student);
+        tr.commit();
+    }
+    
+    
+    
+    public static void delete(Student student){
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        session.delete(student);
+        tr.commit();
+    }
+    
+    public static Student getById(int id){
+        Student student = new Student();
+        try {
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        student = (Student) session.get(Student.class, id);
+        tr.commit();
+        } catch (Exception e) {
+        }
+        return student;
+        
+    }
+    
+    public static List<Student> getList(){
+        List<Student> students = new ArrayList<>();
+        try {
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        students = session.createQuery("FROM Student").list();
+        tr.commit();
+        } catch (Exception e) {
+        }
+        return students;
+    }
+}
